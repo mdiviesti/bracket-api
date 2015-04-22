ece33b0ff9653e8cb165695bd35f78a774ade480
Michael Diviesti <mediviesti@gmail.com>
2015-04-22 01:05:14 -0500

    hooked in MongoDB and added post, put, and delete methods for the teams collection.

--- routes/teams.js
+++ routes/teams.js
@@ -1,7 +1,77 @@
-exports.findAll = function(req, res) {
-    res.send([{name:'Battle Wolves'}, {name:'Rough Riders'}, {name:'Polar Bears'}]);
-};
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure,
+    dbName = 'teamsdb',
+    collectionName = 'teams';
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db(dbName, server);
+
+db.open(function(err, db) {
+    if(!err) {
+        db.collection(collectionName, {strict:true}, function(err, collection) {
+            if (err) {
+
+            }
+        });
+    }
+});
 
 exports.findById = function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
+    var id = req.params.id;
+    db.collection(collectionName, function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
 };
+
+exports.findAll = function(req, res) {
+    db.collection(collectionName, function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addTeam = function(req, res) {
+    var team = req.body;
+    db.collection(collectionName, function(err, collection) {
+        collection.insert(team, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.updateTeam = function(req, res) {
+    var id = req.params.id;
+    var team = req.body;
+    db.collection(collectionName, function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, team, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(team);
+            }
+        });
+    });
+}
+
+exports.deleteTeam = function(req, res) {
+    var id = req.params.id;
+    db.collection(collectionName, function (err, collection) {
+        collection.remove({'_id': new BSON.ObjectID(id)}, {safe: true}, function (err, result) {
+            if (err) {
+                res.send({'error': 'An error has occurred - ' + err});
+            } else {
+                res.send(req.body);
+            }
+        });
+    });
+}
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express'),
     teams = require('./routes/teams'),
-    players = require('./routes/players'),
-    index = require('./routes/index');
+    players = require('./routes/players');
 
 var app = express();
 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
 app.get('/teams', teams.findAll);
 app.get('/teams/:id', teams.findById);
+app.post('/teams', teams.addTeam);
+app.put('/teams/:id', teams.updateTeam);
+app.delete('/teams/:id', teams.deleteTeam);
 app.get('/players', players.findAll);
 app.get('/players/:id', players.findById);
-app.get('*', index.index);
+app.use(function(req, res){
+    res.status(404).json({response: {status: '404', message: 'you are attempting to use a route that has not been defined'}});
+});
 app.listen(3000);
 console.log('Listening on port 3000...');
